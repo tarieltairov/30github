84d8425f70b510340f9858ced657113949057a83
Tariel <tarieltairov1@gmail.com>
2025-02-11 10:54:35 +0600

    second problem

--- index.js
+++ index.js
@@ -22,3 +22,28 @@
 
 // clearDigits("cb34");
 // // clearDigits("abc");
+// ------------------------------------------------------
+
+// 2
+// var twoSum = function (nums, target) {
+//   if (nums.length > 2) {
+//     for (let i = 0; i < nums.length; i++) {
+//       for (let j = i + 1; j < nums.length; j++) {
+//         if (nums[i] + nums[j] === target) {
+//           return [i, j];
+//         }
+//       }
+//     }
+//   }
+
+//   if (nums.length === 2) {
+//     if (nums[0] + nums[1] === target) {
+//       return [0, 1];
+//     }
+//   }
+//   return;
+// };
+
+// console.log(twoSum([2, 7, 11, 15], 9));
+// console.log(twoSum([3, 2, 4], 6));
+// console.log(twoSum([3, 3], 6));
