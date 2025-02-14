7d07f4d88b7d96272106708a64291223126459c0
Tariel <tarieltairov1@gmail.com>
2025-02-14 18:08:29 +0600

    expect problem

---
+++ problems/expect.js
@@ -0,0 +1,16 @@
+// var expect = function (val) {
+//   return {
+//     toBe: (expected) => {
+//       if (val === expected) return true;
+//       throw new Error("Not Equal");
+//     },
+//     notToBe: (expected) => {
+//       if (val !== expected) return true;
+//       throw new Error("Equal");
+//     },
+//   };
+// };
+
+// console.log(expect(5).toBe(5));
+
+// console.log(expect(5).notToBe(5));
