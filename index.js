ca62b7fbb5c6dc6cb50e125b11a285554f1f48db
Tariel <tarieltairov1@gmail.com>
2025-02-15 23:37:43 +0600

    easy func

--- index.js
+++ index.js
@@ -47,3 +47,10 @@
 // console.log(twoSum([2, 7, 11, 15], 9));
 // console.log(twoSum([3, 2, 4], 6));
 // console.log(twoSum([3, 3], 6));
+
+// -----------------------------------
+// var createHelloWorld = function () {
+//   return function (...args) {
+//     return "Hello World";
+//   };
+// };
