600903cbbbc13bdb85b3d4a3b14c9a02b500fcc2
Tariel <tarieltairov1@gmail.com>
2025-02-23 20:04:01 +0600

    composition func - reduceRight

---
+++ problems/compositionFunc.js
@@ -0,0 +1,8 @@
+// var compose = function (functions) {
+//   return function (x) {
+//     return functions.reduceRight((acc, fn) => fn(acc), x);
+//   };
+// };
+
+// const fn = compose([(x) => x + 1, (x) => 2 * x]);
+// console.log(fn(4)); // 9
